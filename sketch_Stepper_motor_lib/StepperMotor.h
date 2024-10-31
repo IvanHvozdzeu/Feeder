336dd64e1fb3a43ebce1cb68c3d7c825743f74c4
IvanHvozdzeu <108997803+IvanHvozdzeu@users.noreply.github.com>
2024-10-31 09:37:59 +0100

    StepperMotor.h

--- sketch_Stepper_motor_lib/StepperMotor.h
+++ sketch_Stepper_motor_lib/StepperMotor.h
@@ -4,9 +4,9 @@ class StepperMotor
 {
   public:
     StepperMotor(int pin1, int pin2, int pin3, int pin4);
-    vibroMovement(int right,int left);
-    clockwise();
-    anticlockwise();
+    void vibroMovement(int right,int left);
+    void clockwise();
+    void anticlockwise();
     int motorSpeed = 1200;
   private:
     int _motorPin1;    // Blue   - 28BYJ48 pin 1
@@ -14,5 +14,5 @@ class StepperMotor
     int _motorPin3;    // Yellow - 28BYJ48 pin 3
     int _motorPin4;    // Orange - 28BYJ48 pin 4
     int _lookup[8] = {B01000, B01100, B00100, B00110, B00010, B00011, B00001, B01001}; /////////Массив со значениями (8,12,4,6,2,3,1,9)
-    setOutput(int out); 
+    void setOutput(int out); 
   };
