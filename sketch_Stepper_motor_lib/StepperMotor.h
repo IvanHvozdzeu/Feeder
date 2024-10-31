52c9dc6fcdaf237a47640bbfb0e65a0d8c05ce20
IvanHvozdzeu <108997803+IvanHvozdzeu@users.noreply.github.com>
2024-10-31 12:13:40 +0100

    StepperMotor.h

--- sketch_Stepper_motor_lib/StepperMotor.h
+++ sketch_Stepper_motor_lib/StepperMotor.h
@@ -13,6 +13,6 @@ class StepperMotor
     int _motorPin2;    // Pink   - 28BYJ48 pin 2
     int _motorPin3;    // Yellow - 28BYJ48 pin 3
     int _motorPin4;    // Orange - 28BYJ48 pin 4
-    int _lookup[8] = {B01000, B01100, B00100, B00110, B00010, B00011, B00001, B01001}; /////////Массив со значениями (8,12,4,6,2,3,1,9)
+    int _lookup[8] = {B01000, B01100, B00100, B00110, B00010, B00011, B00001, B01001}; /////////Полушаговый режим для моторчика
     void setOutput(int out); 
   };
