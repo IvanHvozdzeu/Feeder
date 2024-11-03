f938fe1e482467ca0f1a5e7d95636d3d2fa5808a
IvanHvozdzeu <108997803+IvanHvozdzeu@users.noreply.github.com>
2024-11-03 23:32:47 +0100

    Update sketch_Button_lib.ino

--- sketch_Button_lib/sketch_Button_lib.ino
+++ sketch_Button_lib/sketch_Button_lib.ino
@@ -1,48 +1,64 @@
-uint8_t pin = 8;
-unsigned long _tmr = 0; 
-bool _flag = digitalRead(pin);
-uint32_t _btnTimer = millis();
-void setup() {
- Serial.begin (9600);
- pinMode(8,INPUT);
-
 }
 void loop() 
 {
- _flag = digitalRead(pin);
- clickType();
- Serial.println(_btnTimer);
+ switch(button.clickType())
+ {
+  case 1:
+  timer=millis();
+  while(millis() - timer < button.holdTimer)
+  {
+  motor.vibroMovement(5,3);
+  }
+  break;
+  case 2:
+  timer=millis();
+  while(millis() - timer < button.holdTimer)
+  {
+  motor.anticlockwise();
+  }
+  break;
+  case 4:
+  motor.vibroMovement(5, 3);
+ }
 }
-uint8_t clickType()
+/* uint8_t clickType()
 {
- switch(_flag)
+ switch(digitalRead(pin))
  {
   case 0:
-   Serial.println("Нет нажатия");
-   if(1000 > _btnTimer > 0 )
+   if(!_pressFlag && _holdFlag && millis() - _btnTimer > 100 )
    {
+    _btnTimer = millis();
+    _holdFlag = false;
+    return 0;
+   }
+   if(_pressFlag && _holdFlag)
+   {
+    _btnTimer = millis();
+    _pressFlag = false;
+    Serial.println("Нажатие");
     return 1;
    }
    else
    {
-    Serial.println(0);
     return 0;
    }
-   _btnTimer = 0;
-   _tmr = millis();
    break;
   case 1:
-   Serial.println("Нажатие");
-   _btnTimer = millis();
-/*    for(uint8_t i= 0; i>=0; i++)
+   if(!_holdFlag && millis() - _btnTimer > 100 )
    {
-    _btnTimer = (_btnTimer + i);
-    if(_btnTimer >= 1000)
-    {
+    //Serial.println("Нажатие");
+    _btnTimer = millis();
+    _holdFlag = true;
+    _pressFlag = true;
+   }
+   if(_holdFlag && millis() - _btnTimer > 1000 )
+   {
     Serial.println("Удержание");
-     return 2;
-    }
-   } */
+    _btnTimer = millis();
+    _pressFlag = false;
+    return 2;
+   } 
    break;
  }
-}
+} */
