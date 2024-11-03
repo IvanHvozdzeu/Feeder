a3524e82fa1555b75e77fe27ace3bfd9333a4205
IvanHvozdzeu <108997803+IvanHvozdzeu@users.noreply.github.com>
2024-11-03 11:47:14 +0100

    Update StepperMotor.h

--- sketch_Stepper_motor_lib/StepperMotor.h
+++ sketch_Stepper_motor_lib/StepperMotor.h
@@ -1,18 +1,27 @@
-#pragma once
+ifndef StepperMotor_h
+#define StepperMotor_h
+
 #include "Arduino.h"
+
 class StepperMotor
 {
   public:
-    StepperMotor(int pin1, int pin2, int pin3, int pin4);
-    void vibroMovement(int right,int left);
+    StepperMotor(uint8_t pin1, uint8_t pin2, uint8_t pin3, uint8_t pin4);
+    void vibroMovement(uint8_t right,uint8_t left);
     void clockwise();
     void anticlockwise();
-    int motorSpeed = 1200;
+    void _setOutput(uint8_t out);
+    uint8_t motorSpeed = 1000;
+    
   private:
-    int _motorPin1;    // Blue   - 28BYJ48 pin 1
-    int _motorPin2;    // Pink   - 28BYJ48 pin 2
-    int _motorPin3;    // Yellow - 28BYJ48 pin 3
-    int _motorPin4;    // Orange - 28BYJ48 pin 4
-    int _lookup[8] = {B01000, B01100, B00100, B00110, B00010, B00011, B00001, B01001}; /////////Полушаговый режим для моторчика
-    void setOutput(int out); 
+   uint32_t _clcTimer;
+   uint8_t _motorPin1;
+   uint8_t _motorPin2;    
+   uint8_t _motorPin3;    
+   uint8_t _motorPin4;    
+   uint8_t _lookup[8] = {B01000, B01100, B00100, B00110, B00010, B00011, B00001, B01001};
+   uint8_t _lookupI;
+   uint8_t vibroSteps;    
   };
+
+  #endif
