ed9a4c55ad109899c39ad3b36b33cfa4ba04fa4f
IvanHvozdzeu <108997803+IvanHvozdzeu@users.noreply.github.com>
2024-11-03 15:17:04 +0100

    Update GButton.h

--- sketch_Button_lib/GButton.h
+++ sketch_Button_lib/GButton.h
@@ -6,10 +6,10 @@ class GButton
 {
   public:
    GButton (uint8_t pin);
-   
-  private:
-   bool doubleClick();
    uint8_t click();
+   int doubleClick();
+  private:
+   uint8_t _clickCount;
    uint8_t _pin;
    bool _holdFlag;
    bool _pressFlag;
