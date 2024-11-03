a544c52ab04801da217095bd96d208330824ecf9
IvanHvozdzeu <108997803+IvanHvozdzeu@users.noreply.github.com>
2024-11-03 15:16:41 +0100

    Update GButton.cpp

--- sketch_Button_lib/GButton.cpp
+++ sketch_Button_lib/GButton.cpp
@@ -5,43 +5,71 @@ GButton::GButton(uint8_t pin)
 {
  _pin=pin;
 }
-///////////////////////////// Флаг для одного клика и удержания
+///////////////////////////// Флаг для одного клика или удержания
 uint8_t GButton::click()
 {
  switch(digitalRead(_pin))
  {
   case 0:
-   if(!_pressFlag && _holdFlag && millis() - _btnTimer > 100 )
+   if(!_pressFlag && _holdFlag && millis() - _btnTimer > 50 ) /// Сброс флагов
    {
     _btnTimer = millis();
     _holdFlag = false;
+    return 0;
    }
-   if(_pressFlag && _holdFlag)
+   if(_pressFlag && _holdFlag) /// Нажатие на кнопку с удержанием 0,1 сек
    {
     _btnTimer = millis();
     _pressFlag = false;
-    Serial.println("Нажатие");
+    _clickCount ++;
    }
-   else
+   if (millis() - _btnTimer > 200)
+   {
+    if (_clickCount > 3)
+    {
+      _clickCount = 3;
+    }
+    switch(_clickCount)
+    {
+      case 0:
+      return 0;
+      break;
+      case 1:
+      Serial.println("Нажатие");
+      _clickCount = 0;
+      return 1;
+      break;
+      case 2:
+      Serial.println("Двойное нажатие");
+      _clickCount = 0;
+      return 2;
+      break;
+      case 3:
+      Serial.println("Тройное нажатие");
+      _clickCount = 0;
+      return 3;
+      break;
+    }
+    _clickCount = 0;
+   }
+   else /// Отсутствие нажатия
    {
     return 0;
    }
    break;
   case 1:
-   if(!_holdFlag && millis() - _btnTimer > 100 )
+   if(!_holdFlag && millis() - _btnTimer > 50 ) /// Регистрация нажатия. Ничего не возвращает т.к. функция ждёт будет ли удержание или просто нажатие
    {
-    //Serial.println("Нажатие");
     _btnTimer = millis();
-    _holdFlag = true;
-    _pressFlag = true;
-    return 1;
+    _holdFlag = true; /// флаг удержания
+    _pressFlag = true; /// флаг нажатия
    }
-   if(_holdFlag && millis() - _btnTimer > 1000 )
+   if(_holdFlag && millis() - _btnTimer > 500 ) /// Удержание кнопки более 1 сек.
    {
     Serial.println("Удержание");
     _btnTimer = millis();
     _pressFlag = false;
-    return 2;
+    return 4;
    } 
    break;
  }
