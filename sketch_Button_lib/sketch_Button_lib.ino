4e5f5dbcc842521baf192a58f7ef058e70a64c0f
IvanHvozdzeu <108997803+IvanHvozdzeu@users.noreply.github.com>
2024-11-17 12:26:10 +0100

    Update sketch_Button_lib.ino

--- sketch_Button_lib/sketch_Button_lib.ino
+++ sketch_Button_lib/sketch_Button_lib.ino
@@ -1,3 +1,16 @@
+#include "GButton.h"
+#include "StepperMotor.h"
+
+uint8_t pin = 7;
+GButton button(pin);
+StepperMotor motor(8,9,10,11);
+uint32_t timer; 
+
+void setup() 
+{
+
+ Serial.begin (9600);
+ pinMode(8,INPUT);
 }
 void loop() 
 {
@@ -25,6 +38,43 @@ void loop()
 {
  switch(digitalRead(pin))
  {
+  case 0:
+   if(!_pressFlag && _holdFlag && millis() - _btnTimer > 100 )
+   {
+    _btnTimer = millis();
+    _holdFlag = false;
+    return 0;
+   }
+   if(_pressFlag && _holdFlag)
+   {
+    _btnTimer = millis();
+    _pressFlag = false;
+    Serial.println("Нажатие");
+    return 1;
+   }
+   else
+   {
+    return 0;
+   }
+   break;
+  case 1:
+   if(!_holdFlag && millis() - _btnTimer > 100 )
+   {
+    //Serial.println("Нажатие");
+    _btnTimer = millis();
+    _holdFlag = true;
+    _pressFlag = true;
+   }
+   if(_holdFlag && millis() - _btnTimer > 1000 )
+   {
+    Serial.println("Удержание");
+    _btnTimer = millis();
+    _pressFlag = false;
+    return 2;
+   } 
+   break;
+ }
+} */
   case 0:
    if(!_pressFlag && _holdFlag && millis() - _btnTimer > 100 )
    {
