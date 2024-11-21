982943dd0bfed57fba4757529a96bfbf97183a66
IvanHvozdzeu <108997803+IvanHvozdzeu@users.noreply.github.com>
2024-11-21 21:14:36 +0100

    Update sketch_Button_lib_copy_20241118221020.ino

--- sketch_Button_lib_copy_20241118221020/sketch_Button_lib_copy_20241118221020.ino
+++ sketch_Button_lib_copy_20241118221020/sketch_Button_lib_copy_20241118221020.ino
@@ -46,7 +46,7 @@ void setup()
 }
 void loop() {
 //////////////////////////////////////////////DHT11 TEST	
-int temperature = 0;
+/* int temperature = 0;
 int humidity = 0;
 int result = dht11.readTemperatureHumidity(temperature, humidity);
 if (result == 0) 
@@ -61,13 +61,14 @@ if (result == 0)
  {
         // Print error message based on the error code.
         Serial.println(DHT11::getErrorString(result));
- }
+ } */
 /////////////////////////////////////////////	
 dateInit();
 timeInit();
 if(setTimeFlag == 0)
 {
   timeViev();
+  dateViev();
 }
 if(button2.clickType() == 4 && button1.clickType() == 4)
 {
@@ -89,6 +90,19 @@ void timeViev()
   lcd.print(sec[1], DEC);
 }
 //////////////////////////////////
+void dateViev()
+{
+  lcd.setCursor(8,1);
+  lcd.print(day[0], DEC);
+  lcd.print(day[1], DEC);
+	lcd.print("/");
+	lcd.print(mon[0], DEC);
+  lcd.print(mon[1], DEC);
+	lcd.print("/");
+	lcd.print(year[0], DEC);
+  lcd.print(year[1], DEC);
+}
+//////////////////////////////////
 void resetTime()
 {
   bool timeStop = 1;
@@ -99,7 +113,7 @@ void resetTime()
   RTC.setSecond(1);
   }
 }
-////////////////////////////////
+//////////////////////////////// Инициализация установки времени.
 void setTime()
 {
  uint8_t cursor = 0;
@@ -120,48 +134,41 @@ void setTime()
    {
     case 0:
 
-     _hour[0]++;
-     if (_hour[0] > 2){_hour[0]=0;}
+     _hour[0] = (_hour[0] + 1) % 3;
      lcd.print(_hour[0]);
      lcd.setCursor(cursor, 0);
      break;
     case 1:
     
-     _hour[1]++;
-     if (_hour[1] > 9){_hour[1] = 0;}
-     if (_hour[1] > 4 && _hour[0] == 2){_hour[1]=0;}
+     _hour[1] = (_hour[1] + 1) % (_hour[0] == 2 ? 4 : 10);
      lcd.print(_hour[1]);
      lcd.setCursor(cursor, 0);
      break;
     
     case 3 :
     
-     _min[0]++;
-     if (_min[0] > 6){_min[0] = 0;}
+     _min[0] = (_min[0] + 1) % 6;
      lcd.print(_min[0]);
      lcd.setCursor(cursor, 0);
      break;
     
     case 4 :
     
-      _min[1]++;
-      if (_min[1] > 9){_min[1] = 0;}
+      _min[1] = (_min[1] + 1) % 10;
       lcd.print(_min[1]);
       lcd.setCursor(cursor, 0);
       break;
     
     case 6 :
     
-      _sec[0]++;
-      if (_sec[0] > 6){_sec[0] = 0;}
+      _sec[0] = (_sec[0] + 1) % 6;
       lcd.print(_sec[0]);
       lcd.setCursor(cursor, 0);
       break;
     
     case 7 :
     
-      _sec[1]++;
-      if (_sec[1] > 9){_sec[1] = 0;}
+      _sec[1] = (_sec[1] + 1) % 10;
       lcd.print(_sec[1]);
       lcd.setCursor(cursor, 0);
       break;
@@ -187,7 +194,7 @@ void setTime()
  RTC.setSecond(_sec[0]*10+_sec[1]);
  setTimeFlag = 0;
 }
-///////////////////////////////////////
+/////////////////////////////////////// Инициализация времени в формате 24h hh:mm:ss
 void timeInit()
 {
  hour[0] = RTC.getHour(h12Flag, pmFlag) / 10;
@@ -197,7 +204,7 @@ void timeInit()
  sec[0] = RTC.getSecond() / 10;
  sec[1] = RTC.getSecond() % 10;
 }
-///////////////////////////////////////
+/////////////////////////////////////// инициализация даты в формате dd/mm/yy
 void dateInit()
 {
  day[0] = RTC.getDate() / 10;
